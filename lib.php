02d0867a3951ca73428d3146a1134066bd56aa0c
Renzo Medina <medinast30@gmail.com>
2023-10-07 01:30:57 -0300

    Display a localised greeting to user

--- lib.php
+++ lib.php
@@ -22,18 +22,24 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once('../../config.php');
-require_once($CFG->dirroot. '/local/greetings/lib.php');
-$context = context_system::instance();
-$PAGE->set_context($context);
-$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
-$PAGE->set_pagelayout('standard');
-$PAGE->set_title($SITE->fullname);
-$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
-echo $OUTPUT->header();
-if (isloggedin()) {
-    echo local_greetings_get_greeting($USER);
-} else {
-    echo get_string('greetinguser', 'local_greetings');
+function local_greetings_get_greeting($user) {
+    if ($user == null) {
+        return get_string('greetinguser', 'local_greetings');
+    }
+
+    $contry = $user->country;
+    switch ($contry) {
+        case 'ES':
+            $langstr = 'greetinguseres';
+            break;
+        case 'AU':
+            $langstr = 'greetinguserau';
+            break;
+        default:
+            $langstr = 'greetingloggedinuser';
+        break;
+
+    }
+
+    return get_string($langstr, 'local_greetings', fullname($user));
 }
-echo $OUTPUT->footer();
