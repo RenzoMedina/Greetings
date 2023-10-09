4bf0733c950790c7d2db04ad6d4c6752851e1af5
Renzo Medina <medinast30@gmail.com>
2023-10-08 23:56:59 -0300

    Record author of post in the greetings table

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ if ($data = $messageform->get_data()) {
         $record = new stdClass;
         $record->message = $message;
         $record->timecreated = time();
+        $record->userid = $USER->id;
 
         $DB->insert_record('local_greetings_messages', $record);
     }
@@ -49,13 +50,22 @@ if (isloggedin()) {
     echo get_string('greetinguser', 'local_greetings');
 }
 $messageform->display();
-$messages = $DB->get_records('local_greetings_messages');
+$userfields = \core_user\fields::for_name()->with_identity($context);
+$userfieldssql = $userfields->get_sql('u');
+
+$sql = "SELECT m.id, m.message, m.timecreated, m.userid {$userfieldssql->selects}
+          FROM {local_greetings_messages} m
+     LEFT JOIN {user} u ON u.id = m.userid
+      ORDER BY timecreated DESC";
+
+$messages = $DB->get_records_sql($sql);
 echo $OUTPUT->box_start('card_columns');
 echo html_writer::start_tag('div', array('class' => 'd-flex justify-content-between'));
 foreach ($messages as $m) {
     echo html_writer::start_tag('div', array('class' => 'card'));
     echo html_writer::start_tag('div', array('class' => 'card-body'));
     echo html_writer::tag('p', $m->message, array('class' => 'card-text'));
+    echo html_writer::tag('p', get_string('postedby', 'local_greetings', $m->firstname), array('class' => 'card-text'));
     echo html_writer::start_tag('p', array('class' => 'card-text'));
     echo html_writer::tag('small', userdate($m->timecreated), array('class' => 'text-muted'));
     echo html_writer::end_tag('p');
